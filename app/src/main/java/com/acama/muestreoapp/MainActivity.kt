73d414ccc83f3134da1bd84ff68cb76b19d76744
IsaacVillanuevaAlonso <isaacyannis@gmail.com>
2021-06-03 13:31:15 -0500

    Commit Creación le login y prueba de sincronización

--- app/src/main/java/com/acama/muestreoapp/MainActivity.kt
+++ app/src/main/java/com/acama/muestreoapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.acama.muestreoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.sax.StartElementListener
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnEntrar = findViewById<Button>(R.id.btnEntrar) as Button
+
+        btnEntrar.setOnClickListener{
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
     }
 }
