5617ee12fbbd328668962432b33ac612c56499c1
LashGAMX <alber_199648@hotmail.com>
2021-06-07 10:41:21 -0500

    Imagenes icons

---
+++ app/src/main/java/com/acama/muestreoapp/AguaCapturaActivity.kt
@@ -0,0 +1,11 @@
+package com.acama.muestreoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AguaCapturaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agua_captura)
+    }
+}
