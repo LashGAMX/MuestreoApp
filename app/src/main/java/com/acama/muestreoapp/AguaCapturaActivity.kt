651e120ea9499318f171af4567bae67ee612c1c8
LashGAMX <alber_199648@hotmail.com>
2021-06-07 12:00:06 -0500

    Captura de datos

--- app/src/main/java/com/acama/muestreoapp/AguaCapturaActivity.kt
+++ app/src/main/java/com/acama/muestreoapp/AguaCapturaActivity.kt
@@ -1,11 +1,23 @@
 package com.acama.muestreoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.acama.muestreoapp.databinding.ActivityAguaCapturaBinding
 
 class AguaCapturaActivity : AppCompatActivity() {
+    private  lateinit var bin: ActivityAguaCapturaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_agua_captura)
+        bin = ActivityAguaCapturaBinding.inflate(layoutInflater)
+        setContentView(bin.root)
+
+        bin.btnGenerales.setOnClickListener { generales() }
+    }
+
+    fun generales()
+    {
+        val intent = Intent(this,AguaGeneralesActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/acama/muestreoapp/AguaGeneralesActivity.kt
@@ -0,0 +1,11 @@
+package com.acama.muestreoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AguaGeneralesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agua_generales)
+    }
+}
