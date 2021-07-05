1be4853dc08caf76e3408dc1b347e822e897d76f
IsaacVillanuevaAlonso <isaacyannis@gmail.com>
2021-07-05 17:53:13 -0500

    commit creación de tabla

---
+++ app/src/main/java/com/acama/muestreoapp/Generales.kt
@@ -0,0 +1,21 @@
+package com.acama.muestreoapp
+
+class Generales {
+    var Id_general: Int = 0
+    var Id_solicitud: Int = 0
+    var Captura: String = ""
+    var Id_equipo: Int = 0
+    var Temperatura_a: Int = 0
+    var Temperatura_b: Int = 0
+    var Latitud: String = ""
+    var Longitud: String = ""
+    var Altitud: String = ""
+    var Pendiente: String = ""
+    var Criterio: String = ""
+
+    constructor(Id_general: Int, Id_solicitud: Int, Captura: String, Id_equipo: Int, Temperatura_a: Int,
+                Temperatura_b: Int, Latitud: String, Longitud: String, Altitud: String, Pendiente: String,
+                Criterio: String){
+
+    }
+}
