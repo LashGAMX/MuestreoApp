849f312c0c6f4929f612be4377ea17e3674b66fd
LashGAMX <alber_199648@hotmail.com>
2021-07-16 11:09:38 -0500

    commit

--- app/src/main/java/com/acama/muestreoapp/AguaDatosMuestreosActivity.kt
+++ app/src/main/java/com/acama/muestreoapp/AguaDatosMuestreosActivity.kt
@@ -2,16 +2,28 @@ package com.acama.muestreoapp
 
 import android.app.AlertDialog
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.acama.muestreoapp.agua.MuestraSimple
+import com.acama.muestreoapp.agua.MuestraSimpleAdapter
 import com.acama.muestreoapp.databinding.ActivityAguaCapturaBinding
 import com.acama.muestreoapp.databinding.ActivityAguaDatosMuestreosBinding
+import com.acama.muestreoapp.preference.UserApplication
 
 class AguaDatosMuestreosActivity : AppCompatActivity() {
     private  lateinit var bin: ActivityAguaDatosMuestreosBinding
     private  lateinit var folio:String
+    private  lateinit var con: DataBaseHandler
+    var muestraSimple = listOf(
+        MuestraSimple(1),
+        MuestraSimple(2)
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bin = ActivityAguaDatosMuestreosBinding.inflate(layoutInflater)
@@ -19,15 +31,24 @@ class AguaDatosMuestreosActivity : AppCompatActivity() {
 
         getExtras()
 
-        bin.btnMS1.setOnClickListener(View.OnClickListener { v: View? ->
-            val intent = Intent(this, MuestraSimpleActivity::class.java)
-            startActivity(intent)
-        })
+
         bin.imgRegresar.setOnClickListener(View.OnClickListener { v: View? ->
             DialogVolver()
         })
 
+        initRecycler()
     }
+
+    fun getNumTomas(){
+
+    }
+
+    fun initRecycler(){
+        bin.rvMuestraSimple.layoutManager = LinearLayoutManager(this)
+        val adapter = MuestraSimpleAdapter(muestraSimple)
+        bin.rvMuestraSimple.adapter = adapter
+    }
+
     fun getExtras(){
         val bundle = intent.extras
         val fol = bundle?.get("folio")
--- app/src/main/java/com/acama/muestreoapp/agua/MuestraSimpleAdapter.kt
+++ app/src/main/java/com/acama/muestreoapp/agua/MuestraSimpleAdapter.kt
@@ -1,14 +1,37 @@
 package com.acama.muestreoapp.agua
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.acama.muestreoapp.R
+import com.acama.muestreoapp.databinding.ItemMuestrasimpleBinding
 
 class MuestraSimpleAdapter (val muestrasimple: List<MuestraSimple>) : RecyclerView.Adapter<MuestraSimpleAdapter.MuestraSimpleHolder>() {
 
     class MuestraSimpleHolder(val view: View):RecyclerView.ViewHolder(view){
 
-        private val bin = Item
+        private val bin = ItemMuestrasimpleBinding.bind(view)
+        fun render(muestrasimple:MuestraSimple){
+            bin.txtMuestra.text = "Muestra simple"
+            bin.txtNumeroMuestra.text = muestrasimple.numToma.toString()
+            //bin.edtColor.setBackgroundColor(R.drawable._11_computer)
+            view.setOnClickListener { Toast.makeText(view.context,"Has seleccionado ${muestrasimple.numToma}",Toast.LENGTH_SHORT).show() }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MuestraSimpleHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+       return MuestraSimpleHolder(layoutInflater.inflate(R.layout.item_muestrasimple,parent,false))
 
     }
+
+    override fun onBindViewHolder(holder: MuestraSimpleHolder, position: Int) {
+        holder.render(muestrasimple[position])
+    }
+
+    override fun getItemCount(): Int  = muestrasimple.size
+
 
 }
