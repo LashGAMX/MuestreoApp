bc323d3f19fd5baae277453b8543244143798fb1
IsaacVillanuevaAlonso <isaacyannis@gmail.com>
2021-08-09 10:45:42 -0500

    commit imagenes completado

--- app/src/main/java/com/acama/muestreoapp/AguaEvidenciaActivity.kt
+++ app/src/main/java/com/acama/muestreoapp/AguaEvidenciaActivity.kt
@@ -12,12 +12,21 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.acama.muestreoapp.databinding.ActivityAguaEvidenciaBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_agua_evidencia.*
+import java.util.HashMap
 
 class AguaEvidenciaActivity : AppCompatActivity() {
 
     private val SELECT_ACTIVITY = 50
     private var imageUri: Uri? = null
+
+    private val File = 1
+    private val database = Firebase.database
+    val myRef = database.getReference("user")
 
     private lateinit var bin: ActivityAguaEvidenciaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,8 +39,17 @@ class AguaEvidenciaActivity : AppCompatActivity() {
             DialogVolver()
         })
         bin.img1.setOnClickListener{
-            ImageController.selectPhotoFromGallery(this, SELECT_ACTIVITY)
+           //ImageController.selectPhotoFromGallery(this, SELECT_ACTIVITY)
+            fileUpload()
         }
+        bin.btHecho.setOnClickListener{
+            onBackPressed()
+        }
+    }
+    fun fileUpload() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, File)
     }
     fun DialogVolver(){
         val builder = AlertDialog.Builder(this)
@@ -52,16 +70,38 @@ class AguaEvidenciaActivity : AppCompatActivity() {
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        when {
-            requestCode == SELECT_ACTIVITY && resultCode  == Activity.RESULT_OK -> {
-                imageUri = data!!.data
-
-               bin.img1.setImageURI(imageUri)
-
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+////        super.onActivityResult(requestCode, resultCode, data)
+////
+////        when {
+////            requestCode == SELECT_ACTIVITY && resultCode  == Activity.RESULT_OK -> {
+////                imageUri = data!!.data
+////
+////               bin.img1.setImageURI(imageUri)
+////
+////            }
+////        }
+////    }
+override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    if (requestCode == File) {
+        if (resultCode == RESULT_OK) {
+            val FileUri = data!!.data
+            val Folder: StorageReference =
+                FirebaseStorage.getInstance().getReference().child("Campo")
+            val file_name: StorageReference = Folder.child("file" + FileUri!!.lastPathSegment)
+            file_name.putFile(FileUri).addOnSuccessListener { taskSnapshot ->
+                file_name.getDownloadUrl().addOnSuccessListener { uri ->
+                    val hashMap =
+                        HashMap<String, String>()
+                    hashMap["link"] = java.lang.String.valueOf(uri)
+                    myRef.setValue(hashMap)
+                    Log.d("Mensaje", "Se subió correctamente")
+                    Toast.makeText(this, "Se subió correctamente", Toast.LENGTH_SHORT).show()
+                    bin.img1.setImageURI(FileUri)
+                }
             }
         }
     }
+}
 }
